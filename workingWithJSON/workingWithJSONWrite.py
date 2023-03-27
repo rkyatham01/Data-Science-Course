df4037e40ad4ffcefc94d58aee11ccc43751825e
rkyatham01 <rishith.kyatham@stonybrook.edu>
2023-03-27 19:46:05 -0400

    Finished making modifications, back on track to studying

--- workingWithJSON/workingWithJSONWrite.py
+++ workingWithJSON/workingWithJSONWrite.py
@@ -14,6 +14,6 @@ book['bob'] = {
 import json
 #takes dictionary as input and dumping it as a string (in JSON format)
 s = json.dumps(book) 
-with open("book.txt", 'w') as f:
+with open('./book.txt', 'w') as f:
     f.write(s) #writes JSON string to a file
 #JSON is a data exchange format (exchanging from python program -> javascript program)
